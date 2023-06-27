054ca23a9f17f5ff5c44749e95aa61a50b7245aa
Jeroen Nouws <info@moes-media.be>
2023-06-27 10:46:41 +0200

    feat(beerio): created project structure and added amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "beerio": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "beerioe6588aa1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "beerLabels": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
